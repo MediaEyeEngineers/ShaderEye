827542abc2e295c91af454d92867385b51366f5f
yuhao <yuhaoloen2@163.com>
2020-07-14 22:05:58 +0800

    添加摄像头检测 通过界面设置摄像头参数 打开摄像头采集数据

---
+++ ShaderEye/dlgsettingcamera.h
@@ -0,0 +1,33 @@
+﻿#ifndef DLGSETTINGCAMERA_H
+#define DLGSETTINGCAMERA_H
+
+#include <QDialog>
+#include <QCamera>
+
+#include <QList>
+#include <QCameraViewfinderSettings>
+class CameraImage;
+namespace Ui {
+class DlgSettingCamera;
+}
+
+class DlgSettingCamera : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit DlgSettingCamera(QWidget *parent = 0);
+    ~DlgSettingCamera();
+
+private slots:
+    void btn_getcamera_list();
+    void btn_pbtn_startcapture_clicked();
+    void on_capture_frame(const QVideoFrame & frame );
+private:
+    Ui::DlgSettingCamera *ui;
+    QCamera *m_selectedcamera;
+    CameraImage * image;
+    QList<QCameraViewfinderSettings> settinglist;
+};
+
+#endif // DLGSETTINGCAMERA_H
