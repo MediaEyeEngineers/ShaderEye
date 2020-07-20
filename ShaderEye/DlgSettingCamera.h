d9bcac20413bd608e0d2c9b44d1c8cad770e4952
numberwolf <porschegt23@foxmail.com>
2020-07-20 22:37:35 +0800

    update

---
+++ ShaderEye/DlgSettingCamera.h
@@ -0,0 +1,54 @@
+﻿#ifndef DLGSETTINGCAMERA_H
+#define DLGSETTINGCAMERA_H
+
+#include <QDialog>
+#include <QCamera>
+
+#include <QList>
+#include <QCameraViewfinderSettings>
+
+#include "Util/CnmernSelThread.h"
+
+class CameraImage;
+namespace Ui {
+class DlgSettingCamera;
+}
+
+typedef struct MediaInfo {
+    int width;
+    int height;
+    double fps;
+    QVideoFrame::PixelFormat format;
+} MediaInfo;
+
+class DlgSettingCamera : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit DlgSettingCamera(QWidget *parent = 0);
+    ~DlgSettingCamera();
+    const MediaInfo setMediaInfo(int w, int h, int fps, QVideoFrame::PixelFormat format);
+    const MediaInfo getMediaInfo();
+
+private slots:
+    // btn click event
+    void cameraListClick();
+    // callback
+    void recvCaptureFrame(const QVideoFrame & frame);
+
+signals:
+    void readFrame(const uchar *data, qint64 startTime);
+
+private:
+    Ui::DlgSettingCamera *ui;
+    QCamera *m_selectedcamera = nullptr;
+    CameraImage *m_image = nullptr;
+    QList<QCameraInfo> m_cameras;
+    MediaInfo m_mediaInfo;
+
+    uchar *m_frameData = nullptr;
+
+};
+
+#endif // DLGSETTINGCAMERA_H
